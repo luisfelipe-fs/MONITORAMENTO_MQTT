007f1f7ff4d179aed29364045108d3ad390f9500
luisfelipe-fs <51939387+luisfelipe-fs@users.noreply.github.com>
2019-07-23 22:11:25 -0300

    Múltiplos comandos, único publish
    Modificado para funcionar com a versão de múltiplos comandos em um único publish.

--- WebClient/Dashboard/Dashboard.js
+++ WebClient/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
+var DATA_TOPIC = "MASTER/DATA";
+var DATA_REGEX = /[ALPU][GS][01]/g;
 var topics = ["CONTROL/ECP-LAB1", "CONTROL/ECP-LAB2"];
 var enabled = true;
 
-random = Math.floor(Math.random() * 1000).toString();
-client = new Paho.MQTT.Client("iot.eclipse.org", 80, "ECP-MASTER" + random);
+random = Math.floor(Math.random()*1000).toString();
+client = new Paho.MQTT.Client("broker.hivemq.com", 8000, "ECP-MASTER"+random);
 
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
@@ -15,93 +17,22 @@ function publish (topic, msg) {
     client.send(message);
 }
 
-function onConnect() {
-  $("#tr1-light").bootstrapToggle("enable");
-  $("#tr1-air").bootstrapToggle("enable");  
-  $("#tr1-automatic").bootstrapToggle("enable");
-  publish(topics[0], "1GP");
-  publish(topics[0], "1GL");
-  publish(topics[0], "1GA");
-  publish(topics[0], "1GS");
-  console.log("Connected.");
-  client.subscribe("MASTER/DATA");
-  //client.subscribe(topics[0]);
+function enable_toggle () {
+    $("#tr1-light").bootstrapToggle("enable");
+    $("#tr1-air").bootstrapToggle("enable");  
+    $("#tr1-automatic").bootstrapToggle("enable");
+}
+
+function onConnect () {
+    enable_toggle();
+    publish(topics[0], "1PGLGAGUG");
+    console.log("Connected.");
+    client.subscribe(DATA_TOPIC);
 }
 
 function onConnectionLost(responseObject) {
-  console.log("Disconnected.");
-  client.connect({onSuccess:onConnect});
-}
-
-function onMessageArrived(message) {
-  msg = message.payloadString;
-  console.log("TOPIC:"+message.destinationName+"|"+"MSG:"+msg);
-  if (msg.slice(0, 3) == "1GP") {
-      if (msg[3] == "0")
-        $("#tr1-pir").text("Não");
-      else if (msg[3] == "1")
-        $("#tr1-pir").text("Sim");
-  }
-  if (msg.slice(0, 3) == "1GL") {
-      $("#tr1-light").off("change");
-      if (msg[3] == "0") {
-        if (enabled)
-            $("#tr1-light").bootstrapToggle("on");
-        else {
-            $("#tr1-light").bootstrapToggle("enable");
-            $("#tr1-light").bootstrapToggle("on");
-            $("#tr1-light").bootstrapToggle("disable");    
-        }
-      }
-      else if (msg[3] == "1") {
-        if (enabled)
-            $("#tr1-light").bootstrapToggle("off");
-        else {
-            $("#tr1-light").bootstrapToggle("enable");
-            $("#tr1-light").bootstrapToggle("off");
-            $("#tr1-light").bootstrapToggle("disable");
-        }
-      }
-      onToggle("#tr1-light", "1SL0", "1SL1");
-  }
-  if (msg.slice(0, 3) == "1GA") {
-      $("#tr1-air").off("change");
-      if (msg[3] == "0") {
-        if (enabled)
-            $("#tr1-air").bootstrapToggle("on");
-        else {
-            $("#tr1-air").bootstrapToggle("enable");
-            $("#tr1-air").bootstrapToggle("on");
-            $("#tr1-air").bootstrapToggle("disable");
-        }
-      }
-      else if (msg[3] == "1") {
-        if (enabled)
-            $("#tr1-air").bootstrapToggle("off");
-        else {
-            $("#tr1-air").bootstrapToggle("enable");
-            $("#tr1-air").bootstrapToggle("off");
-            $("#tr1-air").bootstrapToggle("disable");
-        }
-      }
-      onToggle("#tr1-air", "1SA0", "1SA1");
-  }
-  if (msg.slice(0, 3) == "1GS") {
-      $("#tr1-automatic").off("change");
-      if (msg[3] == "0") {
-        $("#tr1-automatic").bootstrapToggle("on");
-        $("#tr1-light").bootstrapToggle("enable");
-        $("#tr1-air").bootstrapToggle("enable");
-        enabled = true;
-      }
-      else if (msg[3] == "1") {
-        $("#tr1-automatic").bootstrapToggle("off");
-        $("#tr1-light").bootstrapToggle("disable");
-        $("#tr1-air").bootstrapToggle("disable");
-        enabled = false;
-      }
-      onToggleAuto("#tr1-automatic", "1SS0", "1SS1");
-  }
+    console.log("Disconnected. Reconnecting...");
+    client.connect({onSuccess:onConnect});
 }
 
 function onToggle (id, tmsg, fmsg) {
@@ -110,7 +41,7 @@ function onToggle (id, tmsg, fmsg) {
             publish(topics[0], tmsg);
         else
             publish(topics[0], fmsg);
-    })
+    });
 }
 
 function onToggleAuto (id, tmsg, fmsg) {
@@ -127,5 +58,68 @@ function onToggleAuto (id, tmsg, fmsg) {
             $("#tr1-air").bootstrapToggle("disable");
             enabled = false;
         }
-    })
+    });
+}
+
+function discreteToggle (opt, id, onMsg, offMsg) {
+    $(id).off("change");
+    if (opt == "0") {
+        if (enabled) {
+            $(id).bootstrapToggle("on");
+        }
+        else {
+            $(id).bootstrapToggle("enable");
+            $(id).bootstrapToggle("on");
+            $(id).bootstrapToggle("disable");    
+        }
+    }
+    else if (opt == "1") {
+        if (enabled) {
+            $(id).bootstrapToggle("off");
+        }
+        else {
+            $(id).bootstrapToggle("enable");
+            $(id).bootstrapToggle("off");
+            $(id).bootstrapToggle("disable");
+        }
+    }
+    onToggle(id, onMsg, offMsg);
+}
+
+function onMessageArrived (message) {
+    payload = message.payloadString;
+    console.log("TOPIC:"+message.destinationName+"|"+"MSG:"+payload);
+    commandList = payload.match(DATA_REGEX);
+    for (command of commandList) {
+        if (command.slice(0, 2) == "LG") {
+            discreteToggle(command[2], "#tr1-light", "1LS0", "1LS1");
+        }
+        if (command.slice(0, 2) == "AG") {
+            discreteToggle(command[2], "#tr1-air", "1AS0", "1AS1");
+        }
+        if (command.slice(0, 2) == "PG") {
+            if (command[2] == "0") {
+                $("#tr1-pir").text("Não");
+            }
+            else if (command[2] == "1") {
+                $("#tr1-pir").text("Sim");
+            }
+        }
+        if (command.slice(0, 2) == "UG") {
+            $("#tr1-automatic").off("change");
+            if (command[2] == "0") {
+                $("#tr1-automatic").bootstrapToggle("on");
+                $("#tr1-light").bootstrapToggle("enable");
+                $("#tr1-air").bootstrapToggle("enable");
+                enabled = true;
+            }
+            else if (command[2] == "1") {
+                $("#tr1-automatic").bootstrapToggle("off");
+                $("#tr1-light").bootstrapToggle("disable");
+                $("#tr1-air").bootstrapToggle("disable");
+                enabled = false;
+            }
+            onToggleAuto("#tr1-automatic", "1US0", "1US1");
+        }
+    }
 }
